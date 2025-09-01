a9e3bebad29ffa6da62c8c9628704614d573b2dc
hanifrasendra <133474723+hanifrasendra@users.noreply.github.com>
2025-09-01 20:21:17 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const navLinks = document.querySelectorAll('.nav-link')
+const inHeader = document.querySelector('.inner-header')
+
+navLinks.forEach(nav => {
+    nav.addEventListener('click', () => {
+        //remove all of the 'active' class of the navlink
+        navLinks.forEach(link => link.classList.remove('active'))
+        
+        
+        nav.classList.add('active')
+        
+    })
+})
+
+window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY;
+
+    if(currentScrollY > 100) {
+        inHeader.classList.add('scrolled')
+        navLinks.classList.add('scrolled')
+    } else {
+        inHeader.classList.remove('scrolled')
+        navLinks.classList.remove('scrolled')
+    }
+})
